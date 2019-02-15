4307a9207ba07e600df3323b5ad2da38c8da0977
Jose Luis Martínez Torres <joseluis.martinez@capside.com>
2019-02-15 18:04:39 +0100

    Move CloudFormation::DSL::Object to it's own package

--- lib/CloudFormation/DSL.pm
+++ lib/CloudFormation/DSL.pm
@@ -1,55 +1,12 @@
 use Fn;
 use AWS;
-# Load all Cfn objects from Cfn
-use Cfn;
-use CloudFormation::DSL::Traits;
-package CloudFormation::DSL::Object {
-  use Moose;
-  extends 'Cfn';
-
-  # When the object is instanced, we want any of the attributes declared in the class to be created
-  # That means that attributes with Resource, Output, Condition or Output roles are "attached" to the object
-  # This is done to make the newly created object represent all that the user has declared "in the class" when
-  # they call ->new
-  # All these attributes are normally created with CCfnX::Shortcuts, but can really be created by hand (not recommended)
-  sub BUILD {
-    my $self = shift;
-    my $class_meta = $self->meta;
-    my @attrs = $class_meta->get_all_attributes;
-    foreach my $att (@attrs) {
-      my $name = $att->name;
-      if ($att->does('CloudFormation::DSL::AttributeTrait::Resource')) {
-        $self->addResource($name, $self->$name);
-      } elsif ($att->does('CloudFormation::DSL::AttributeTrait::Output')){
-        $self->addOutput($name, $self->$name);
-      } elsif ($att->does('CloudFormation::DSL::AttributeTrait::Condition')){
-        $self->addCondition($name, $self->$name);
-      } elsif ($att->does('CloudFormation::DSL::AttributeTrait::Mapping')){
-        $self->addMapping($name, $self->$name);
-      } elsif ($att->does('CloudFormation::DSL::AttributeTrait::Metadata')){
-        $self->addMetadata($name, $self->$name);
-      } elsif ($att->does('CloudFormation::DSL::AttributeTrait::Transform')){
-        $self->addTransform($name, $self->$name);
-      }
-    }
-
-    #my $params_meta = $self->params->meta;
-    #@attrs = $params_meta->get_all_attributes;
-    #foreach my $param (@attrs) {
-    #  if ($param->does('CloudFormation::DSL::AttributeTrait::StackParameter')) {
-    #    my $type = $param->type_constraint->name;
-    #    $self->addParameter($param->name, _moose_to_cfn_class($type));
-    #  }
-    #}
-  }
-
-}
 package CloudFormation::DSL {
   use Moose ();
   use Moose::Exporter;
   use Moose::Util::MetaRole ();
 
   use CCfnX::DSL::Inheritance;
+  use CloudFormation::DSL::Object;
 
   Moose::Exporter->setup_import_methods(
     with_meta => [qw/resource/],
