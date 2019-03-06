4b0474c25710a6c7e37d9a445e5e9e606a039722
Jose Luis Martínez Torres <joseluis.martinez@capside.com>
2019-03-06 17:44:21 +0100

    Documentation update

--- lib/CloudFormation/DSL.pm
+++ lib/CloudFormation/DSL.pm
@@ -679,11 +679,15 @@ CloudFormation::DSL - A Domain Specific Language for creating CloudFormation tem
 
 =head1 DESCRIPTION
 
-CloudFormation::DSL is a "framework" for writing CloudFormation in an easier fashion, with a more
-forgiving syntax than the standard JSON or YAML syntaxes that CloudFormation supports. It also
-eases authoring some complex CloudFormation patterns.
+CloudFormation is a great AWS service for automating infrastructure creation. You can get a better
+grasp of what CloudFormation does and tries to solve here: L<https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/Welcome.html>
 
-You can think of it as a preprocessor that generates CloudFormation documents
+CloudFormation::DSL is a "framework" for writing CloudFormation and addressing some of its shortcomings.
+It lets you express CloudFormation templates in an easier fashion, with a more forgiving syntax 
+than the standard JSON or YAML syntaxes that CloudFormation supports. It also eases authoring some 
+complex CloudFormation patterns.
+
+You can think of it as a preprocessor that generates CloudFormation documents.
 
 CloudFormation::DSL builds on the idea that the information in a CloudFormation template can be
 expressed as a class. After all, a class is a template for an object! Since we represent templates 
@@ -691,47 +695,300 @@ as classes, we can instance those classes, manipulate and query them. An instanc
 an C<as_json> method that once called generates the CloudFormation document that can be sent to
 the CloudFormation service.
 
+CloudFormation::DSL builds upon existing layers:
+
+L<Cfn>: Each C<CloudDeploy::DSL> class is a Cfn subclass. This means that the object model that 
+Cfn provides is accessible from C<CloudDeploy::DSL>.
+
+L<Moose> and L<Perl>: C<CloudDeploy::DSL> builds upon Moose's (An Object Orientation framework for Perl)
+and Perl's ability to add syntax to the language. This let's us declare keywords like C<resource> 
+or C<paramter> so you can write your CloudFormation templates in a faster way. This is just an implementation
+detail: you don't need to know Moose or Perl to use C<CloudFormation::DSL>.
+
+CloudFormation::DSL brings you full object orientation to your CloudFormation template authoring: you
+can create base classes, inherit, override and specialize.
+
+Enough chit-chat: let's see the action:
+
 =head1 Writing a class
 
-Start your
+Start a file named `MyCfn.pm`
 
-=head2 resource Name => 'TYPE', { ... Properties ... }[, { ... Resource Attributes ... }]
+  package MyCfn {
+    use CloudFormation::DSL;
+  }
+  1;
+
+You now have a class that represents a template without resources. Now we'll add stuff with
+the following keywords:
+
+=head2 resource Name => 'TYPE', { ... Properties ... };
+
+  package MyCfn {
+    use CloudFormation::DSL;
+
+    resource User1 => 'AWS::IAM::User', {
+      Path => "/",
+    };
+  }
+
+The resource keyword declares a CloudFormation resource of type C<TYPE>. The supported types
+are available in the C<Cfn::Resource> namespace. This piece of code generates the following
+CloudFormation:
+
+  {
+    "Resources": {
+      "User1": {
+        "Type": "AWS::IAM::User",
+	"Properties": {
+          "Path": "/"
+	}
+      }
+    }
+  }
+
+Note a couple of things that the DSL is doing for us:
+
+C<User1> doesn't have to be quoted. Perls "fat comma" automatically quotes it for us!
+
+You could write:
+
+  resource 'User1' => 'AWS::IAM::User', { ... };
+
+if you wanted. You could also use double quotes:
+
+  resource "User1" => 'AWS::IAM::User', { ... };
+
+You actually need to do this if the resource name has special characters. 
+
+Note that we specify the properties just after the object in a Key / Value fashion.
+We use Perls Hashrefs to represent these Key/Value structures. The Keys are the same
+keys that we would use in the C<"Properties"> object in a CloudFormation template. The
+values can be Perl strings C<"myvalue">, Perl numbers C<42>, bareword booleans C<true> 
+or C<false> and also CloudFormation functions like C<{ Ref => 'LogicalId' }> and 
+C<{ 'Fn::GetAtt' =>  [ 'LogicalId', 'AttributeName' ] }> as Perl HashRefs. If you think 
+this is typing too much, please read the "shortcuts" section. You don't have to type
+C<"Properties">, and these go first, since 99% of the time we write resources, we
+write their properties.
+
+Note that in HashRefs we can leave trailing commas:
+
+  { Path => '/', }
+
+is valid, while in JSON
+
+  { "Path": "/", }
+
+isn't valid.
+
+The DSL is also helping us assuring that AWS::IAM::User is a valid resource type. If we
+don't use a supported resource type, we will get an error. This is also true for its
+properties. The C<AWS::IAM::User> object has a property called C<Path>. If we had a
+typo in the C<Path> property, we would get an error. If we didn't define a required property
+we get an error. If we used a wrong value: we get an error.
+
+=head2 resource Name => 'TYPE', { ... Properties ... }, { ... Resource Attributes ... }
+
+If we want to configure resource properties like C<DependsOn> or C<DeletionPolicy>
+we can do so passing a fourth element to our C<resource> statement:
+
+  resource "User1" => 'AWS::IAM::User', { ... }, { DeletionPolicy => 'Retain' };
+
+The DSL will verify that the DeletionPolicy is a permitted value in CloudFormation.
 
 =head2 output Name => ...;
 
+This will declare an output in our CloudFormation template.
+
+  package MyCfn {
+    use CloudFormation::DSL;
+
+    resource User1 => 'AWS::IAM::User', {
+      Path => "/"
+    };
+
+    output IAMUser => Ref('User1');
+  }
+
+Note that the name of the output doesn't need to be quoted (just like in with the
+C<resource> keyword. The value for an output is the same ones that CloudFormation supports.
+In the example we're using a shortcut to specify a CloudFormation Ref function. We could
+have wrote:
+
+  output IAMUser => { Ref => 'User1' };
+
+The two are equivalent to the following CloudFormation JSON:
+
+  {
+    "Outputs": {
+      "IAMUser": {
+        "Value": { "Ref": "User1" }
+      }
+    }
+  }
+
+=head2 output Name => ..., { ... Output properties ... };
+
+We can also specify extra properties for the output C<Description>,
+
+  output IAMUser => { Ref => 'User1' }, {
+    Description => 'The name of the IAM user',
+  };
+
+Will generate:
+
+  {
+    "Outputs": {
+      "IAMUser": {
+        "Value": { "Ref": "User1" },
+        "Description": "The name of the IAM user"
+      }
+    }
+  }
+
 =head2 parameter Name => 'TYPE', { ... Properties ... }
+
+The C<parameter> keyword adds a CloudFormation parameter to the template.
+
+  parameter IAMPath => 'String',
+
+Generates:
+
+  {
+    "Parameters": {
+      "IAMPath": {
+        "Type": "String"
+      }
+    }
+  }
+
+=head2 parameter Name => 'TYPE', { ... Properties ... }
+
+We can also specify parameter properties like C<Default>,
+C<NoEcho>, C<MaxLength>, etc.
+
+  parameter IAMPath => 'String', {
+    Default => '/',
+    MazLength => 32,
+  };
+
+Generates:
+
+  {
+    "Parameters": {
+      "IAMPath": {
+        "Type": "String",
+	"Default": "/",
+	"MaxLength": 32
+      }
+    }
+  }
 
 =head2 condition Name => ...;
 
-=head2 attachment 
+Adds a condition to the template
 
-=head2 mapping
+=head2 mapping Name => { }
+
+Adds a CloudFormation mapping with a specific name
 
 =head2 metadata
 
+Adds a metadata key to the template
+
 =head2 transform
+
+Adds a transform to the template
 
 =head2 stack_version
 
 =head1 Shortcuts
 
-=head2 Ref('ResourceName')
+=head2 Ref('LogicalId')
 
-=head2 GetAtt('ResourceName', 'AttributeName')
+is a shorthand way to write C<{ Ref => 'LogicalId' }>
 
-=head2 UserData($string)
+It writes C<{"Ref":"LogicalId"}> in the CloudFormation template.
 
-=head2 CfString($string)
+=head2 GetAtt('LogicalId', 'AttributeName')
+
+is a shorthand way to write
 
 =head2 Parameter('ParameterName')
 
-=head2 Attribute('AttributeName')
-
-=head2 Json($json_string)
+is a shorthand way of referencing a parameter that doesn't get passed to CloudFormation. 
 
 =head2 Tag($key, $value)
 
+is a shorhand way of writing the values that most C<Tag> attributes of resources expect:
+
+  Tag('Owner', 'me')
+
+gets converted to
+
+  { "Key": "Owner", "Value": "me" }
+
+in CloudFormation
+
+An example of usage would be:
+
+  resource Subnet1 => 'AWS::EC2::Subnet', {
+    VpcId => Ref('Vpc'),
+    CidrBlock => '10.0.0.0/24',
+    Tags => [ Tag('Owner', 'me'), Tag('BU', 'sales') ],
+  };
+
+=head2 Attribute('AttributeName')
+
+is a shorthand way of referencing an instance attribute. This is for advanced use. See
+the "Instance Attributes" section.
+
+=head2 UserData($string)
+
+is a shorthand way to import the contents of a file in "UserData" format:
+  
+=head3 TieFighters
+
+Tiefighters are sequences of C<#-#...#-#> that can be found inside the files
+that the UserData keyword converts to the
+
+=head4 #-#LogicalId#-#
+
+Inserts a Ref('LogicalId')
+
+=head4 #-#LogicalId->Attribute#-#
+
+Inserts a GetAtt('LogicalId', 'Attribute') 
+
+=head4 #-#Parameter(ParamName)#-#
+
+Inserts a Parameter
+
+=head4 #-#Attribute(ParamName)#-#
+
+Inserts an Attribute value
+
+=head2 CfString($string)
+
+is a shorthand way to write a string that has gets tiefighters interpreted
+
+=head2 Json($json_string)
+
+will convert a JSON string into a HashRef
+
+=head1 Networking shortcuts
+
 =head2 ELBListener($lbport, $lbprotocol[, $instance_port[, $instance_protocol]])
+
+is a shorthand way of writing an ELB Listener. It can be used in many ways:
+
+C<ELBListener(80, 'HTTP')> will forward traffic from port 80 of the ELB to port 80 on the backends
+
+C<ELBListener(80, 'HTTP', 3000)> will forward traffic from port 80 of the ELB to port 3000 on the 
+backends
+
+C<ELBListener(443, 'HTTPS', 5000, 'HTTP') will do SSL offloading on the ELB, forwarding to port 
+5000 HTTP on the backends
 
 =head2 TCPELBListener($lbport[, $instance_port])
 
@@ -740,8 +997,6 @@ Start your
 =head2 SGRule($port, $to, $proto, $desc)
 
 =head2 SGEgressRule
-
-=head2 SpecifyInSubClass
 
 =head1 Getting the most out of the DSL
 
@@ -768,8 +1023,39 @@ You can use inheritance primitives to structure your infrastructure into reusabl
     };
   }
 
+=head2 SpecifyInSubClass
+
+Use C<SpecifyInSubClass> to force the user to overwrite this value in a subclass:
+
+  package MyBase {
+    use CloudFormation::DSL;
+
+    resource User1 => 'AWS::IAM::User', {
+      Path => SpecifyInSubclass,
+    };
+  }
+
+If MyBase is instanced, an error will be thrown. The value can be overwritten in 
+subclasses:
+
+  package MySubClass {
+    use CloudFormation::DSL;
+    extends 'MyBase';
+
+    resource '+User1' => 'AWS::IAM::User', {
+      Path => '/',
+    };
+  }
+
 =head1 Attachments
 
+=head2 attachment 
+
+Adds an attachment to the template
+
+=head1 Extending the DSL
+
+=head1 Instance Attributes
 
 =head1 SEE ALSO
 
